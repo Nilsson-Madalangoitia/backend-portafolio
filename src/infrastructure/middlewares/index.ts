8002bf344a6181fa089679115631a3258af2fbba
dhali <FRONT END>
2025-05-02 18:32:46 -0500

    Primera versión del backend del sistema de portafolios: estructura base con Express, MongoDB, TypeScript y JWT. Incluye inicio de sesión, creación de usuarios (rol administrador y docente)

---
+++ src/infrastructure/middlewares/index.ts
@@ -0,0 +1,9 @@
+export * from "./error";
+export * from "./jwt";
+export * from "./not-found";
+export * from "./signin";
+export * from "./signup";
+
+export * from "./validate-user";
+export * from "./validate-rol";
+export * from "./validate-anio";
